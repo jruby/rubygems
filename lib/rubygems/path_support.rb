7bf59fbfcdba7134a47f7b9a905e24c95e05cbc9
Nick Sieger <nick@nicksieger.com>
2011-08-26 11:46:55 -0500

    Reinstate URL path logic in new place in path_support

--- lib/rubygems/path_support.rb
+++ lib/rubygems/path_support.rb
@@ -55,6 +55,17 @@ class Gem::PathSupport
         gem_path = gpaths.dup
       else
         gem_path = gpaths.split(File::PATH_SEPARATOR)
+
+        0.upto(gem_path.size - 1) do |i|
+          # Rejoin incorrectly split URLs. Push proto + ':' onto following item
+          if gem_path[i] =~ /^(jar(:file)?|file|classpath)$/ || # recognize commonly encountered protocols
+              (gem_path[i] =~ /^[a-z]+$/ && # some other protocol and following element begins with '//'
+               gem_path[i+1] && gem_path[i+1] =~ %r{^//})
+            gem_path[i+1] = gem_path[i] + ':' + gem_path[i+1]
+            gem_path[i] = nil
+          end
+        end if File::PATH_SEPARATOR == ':'
+        gem_path.compact!
       end
 
       if File::ALT_SEPARATOR then
